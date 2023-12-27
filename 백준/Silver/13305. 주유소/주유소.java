dd30ca475ef647c8075517189af56fe25cc8923f
yoodab <109517088+yoodab@users.noreply.github.com>
2023-12-27 22:10:07 +0900

    [Silver III] Title: 주유소, Time: 1132 ms, Memory: 184492 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -6,21 +6,21 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 
 		long answer = 0;
-		int min = 10000;
+		
 
 		// 마을 수
 		int n = sc.nextInt();
 
-		int[] distance = new int[n - 1];
-		int[] cost = new int[n];
+		long[] distance = new long[n - 1];
+		long[] cost = new long[n];
 
 		for (int i = 0; i < n-1; i++) {
-			distance[i] = sc.nextInt();
+			distance[i] = sc.nextLong();
 		}
 		for (int i = 0; i < n; i++) {
-			cost[i] = sc.nextInt();
+			cost[i] = sc.nextLong();
 		}
-		
+		long min = cost[0];
 		// 거리*금액을 더함
 		// 금액을 이전 마을들과 비교해 가장 싼 마을에서 기름을 삼
 		for(int i =0; i<n-1;i++) {
