cf82ba85bf0f5604aaca873c625ff824b4265725
yoodab <109517088+yoodab@users.noreply.github.com>
2023-12-27 18:49:16 +0900

    Update A／B.java

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -4,10 +4,14 @@ public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         double a = sc.nextInt();
-        int b = sc.nextInt();
+        double b = sc.nextInt();
 
         System.out.println(a/b);
 
-        
+        // float double => 정밀도 차이
+        // float => 소수점 6~7자리 표현 가능
+        // double => 소수점 15~16자리 표현 가능
+
+        // 코테에서 소수점 표현시 double 사용하는 것이 좋다
     }
 }
