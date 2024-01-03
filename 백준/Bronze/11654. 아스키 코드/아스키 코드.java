d93947930ed042a56ad263b318b35d3c90f06e66
yoodab <109517088+yoodab@users.noreply.github.com>
2024-01-03 21:31:15 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        int Aint = A.charAt(0);
+        System.out.println(Aint);
+    }
+}
