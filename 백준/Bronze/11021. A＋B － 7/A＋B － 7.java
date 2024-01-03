b05f2139ae62375735acaf076412938c8ec3cb21
yoodab <109517088+yoodab@users.noreply.github.com>
2024-01-03 21:18:23 +0900

    [Bronze V] Title: A+B - 7, Time: 252 ms, Memory: 18716 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i=0;i<N;i++){
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            System.out.println("Case #"+(i+1)+": "+(A+B));
+        }
+    }
+}
