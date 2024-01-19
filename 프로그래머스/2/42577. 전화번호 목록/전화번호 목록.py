4318a5ee6f2470a85d81bae5313c65f86e68b145
yoodab <109517088+yoodab@users.noreply.github.com>
2024-01-19 10:58:28 +0900

    [level 2] Title: 전화번호 목록, Time: 90.17 ms, Memory: 28 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,10 @@
+def solution(phone_book):
+    # 전화번호부를 sort를 함 
+    phone_book.sort()
+    answer = True
+    
+    for i in range(1,len(phone_book)):
+        if phone_book[i].startswith(phone_book[i-1]):
+            answer = False
+            break
+    return answer
