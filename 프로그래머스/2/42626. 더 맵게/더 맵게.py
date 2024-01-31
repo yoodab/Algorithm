baacd6c86d5033084f2f4207441019035affe2e0
yoodab <109517088+yoodab@users.noreply.github.com>
2024-01-31 11:23:40 +0900

    [level 2] Title: 더 맵게, Time: 1767.62 ms, Memory: 51.7 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -4,7 +4,6 @@ def solution(scoville, K):
     answer = 0
     heapq.heapify(scoville)
 
-
     while scoville[0] < K:
         if len(scoville)<2:
             answer=-1
@@ -12,9 +11,6 @@ def solution(scoville, K):
 
         h = heapq.heappop(scoville) + heapq.heappop(scoville) * 2
         heapq.heappush(scoville, h)
-
-
         answer += 1
 
-    
     return answer
