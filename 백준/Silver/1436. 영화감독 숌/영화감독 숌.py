6f83f140445e03e46efacf6204613106656583f5
yoodab <109517088+yoodab@users.noreply.github.com>
2024-03-14 16:12:37 +0900

    [Silver V] Title: 영화감독 숌, Time: 660 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,13 @@
+n = int(input())
+num = 666
+count = 0
+while 1:
+    if "666" in str(num):
+        count+=1   
+        
+    if count == n:
+        break
+        
+    num+=1
+    
+print(num)
