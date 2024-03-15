b45fb1ad2285d7160cb102133dabc799178a386c
yoodab <109517088+yoodab@users.noreply.github.com>
2024-03-15 17:51:06 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 4352 ms, Memory: 58044 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -2,7 +2,7 @@ n = int(input())
 p = []
 for _ in range(n):
     p.append(list(map(int,input().split())))
-p.sort(key=lambda x:x[0])
-p.sort(key=lambda x:x[1])
+p.sort(key=lambda x:(x[1],x[0]))
+
 for i in p:
     print(i[0],i[1])
