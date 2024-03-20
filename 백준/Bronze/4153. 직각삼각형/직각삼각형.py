bab61cc2a73e9bc86a5fa246720ff2f26002af26
yoodab <109517088+yoodab@users.noreply.github.com>
2024-03-20 14:48:48 +0900

    [Bronze III] Title: 직각삼각형, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,9 @@
+while(1):
+    l = list(map(int,input().split()))
+    if max(l) == 0:
+        break
+    l.sort()
+    if l[0]**2+l[1]**2==l[2]**2:
+        print("right")
+    else:
+        print("wrong")
