0349cdf14e34c7ebd45f40643a6c11e96d3a155b
yoodab <109517088+yoodab@users.noreply.github.com>
2024-03-21 23:17:27 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 8904 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,10 @@
+import sys
+
+n = int(sys.stdin.readline())
+nums = [0]*10001
+for i in range(n):
+    nums[int(sys.stdin.readline())]+=1
+
+for i in range(10001):
+    for j in range(nums[i]):
+        print(i)
