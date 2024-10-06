61b7bf28b3a54a92df7773a6ae56c8a74739e0e3
yoodab <109517088+yoodab@users.noreply.github.com>
2024-10-06 22:48:05 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,17 +1,9 @@
 class Solution {
-    public boolean solution(String s) {
-        boolean answer = true;
-        
-    	for(char c : s.toCharArray()){
-    		if(!Character.isDigit(c)){
-    			answer = false;
-    			break;
-    		}
-    	}
-        if(s.length()!=4 && s.length()!=6){
-            answer = false;
+        public boolean solution(String s) {
+                    if(s.length() != 4 && s.length() != 6) return false;
+                    for (int i=0;i<s.length();i++) {
+                                    if (s.charAt(i) < '0' || s.charAt(i) > '9') return false;
+                    }        
+                    return true;
         }
-               
-    	return answer;
-    }
 }
